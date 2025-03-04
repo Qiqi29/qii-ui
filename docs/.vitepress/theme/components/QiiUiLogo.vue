963d49290927552b370c47ce601cce7cf666093d
琦琦 <sgq457@qq.com>
2025-03-04 13:12:28 +0800

    文档：点击 logo 可以释放五彩纸屑

--- docs/.vitepress/theme/components/QiiUiLogo.vue
+++ docs/.vitepress/theme/components/QiiUiLogo.vue
@@ -1,5 +1,6 @@
 <script lang="ts" setup>
-import { ref } from 'vue'
+import { onMounted, ref } from 'vue'
+import { confetti } from '@tsparticles/confetti'
 
 const logoRef = ref<HTMLDivElement>()
 const speed = 5
@@ -19,11 +20,31 @@ const handleMoveLeave = () => {
   })
 }
 
+const showConfetti = () => {
+  // 计算logo相对于屏幕的坐标
+  const rect = logoRef.value!.getBoundingClientRect()
+  const elementCenterX = rect.left + rect.width / 2;
+  const elementCenterY = rect.top + rect.height / 2;
+  const mappedX = elementCenterX / window.innerWidth;
+  const mappedY = elementCenterY / window.innerHeight;
+  // 释放纸屑特效
+  confetti({
+    particleCount: 200,
+    angle: 100,
+    startVelocity: 80,
+    spread: 120,
+    origin: { x: mappedX, y: mappedY },
+    ticks: 20,
+    gravity: 1.5,
+    colors: ['#FF8C06','#DD2A2A','#73FFB1','#15BE59','#F48AC3','#FFDC22','#06C3FF'],
+  })
+}
+
 </script>
 
 <template>
   <div class="qii-logo image-src" @mousemove="handleMove" @mouseleave="handleMoveLeave">
-    <img ref="logoRef" src="/favicon.svg" alt="">
+    <img ref="logoRef" src="/favicon.svg" draggable="false" alt="" @click="showConfetti">
   </div>
 </template>
 
